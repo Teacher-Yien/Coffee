861bf75c72e187308cd57cd9894e69a66275a39c
SOTHYIEN <121343858+SOTHYIEN@users.noreply.github.com>
2024-11-13 21:44:37 +0700

    first commit

---
+++ cafeCoffee/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace cafeCoffee
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.panelSilder = new Guna.UI2.WinForms.Guna2Panel();
+            this.btn_logout = new Guna.UI2.WinForms.Guna2Button();
+            this.btn_Custmer = new Guna.UI2.WinForms.Guna2Button();
+            this.btn_products = new Guna.UI2.WinForms.Guna2Button();
+            this.btn_chashier = new Guna.UI2.WinForms.Guna2Button();
+            this.btn_dashboard = new Guna.UI2.WinForms.Guna2Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2ControlBox2 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2ControlBox3 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.panelContainer = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panelSilder.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.guna2ControlBox3);
+            this.panel1.Controls.Add(this.guna2ControlBox2);
+            this.panel1.Controls.Add(this.guna2ControlBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1425, 49);
+            this.panel1.TabIndex = 0;
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.BackColor = System.Drawing.Color.Black;
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(1368, 3);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(54, 43);
+            this.guna2ControlBox1.TabIndex = 0;
+            // 
+            // panelSilder
+            // 
+            this.panelSilder.AutoSize = true;
+            this.panelSilder.BackColor = System.Drawing.Color.Teal;
+            this.panelSilder.Controls.Add(this.btn_logout);
+            this.panelSilder.Controls.Add(this.btn_Custmer);
+            this.panelSilder.Controls.Add(this.btn_products);
+            this.panelSilder.Controls.Add(this.btn_chashier);
+            this.panelSilder.Controls.Add(this.btn_dashboard);
+            this.panelSilder.Controls.Add(this.label1);
+            this.panelSilder.Controls.Add(this.guna2PictureBox1);
+            this.panelSilder.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSilder.Location = new System.Drawing.Point(0, 49);
+            this.panelSilder.Name = "panelSilder";
+            this.panelSilder.Padding = new System.Windows.Forms.Padding(20, 0, 20, 0);
+            this.panelSilder.Size = new System.Drawing.Size(311, 904);
+            this.panelSilder.TabIndex = 1;
+            // 
+            // btn_logout
+            // 
+            this.btn_logout.Animated = true;
+            this.btn_logout.BorderColor = System.Drawing.Color.White;
+            this.btn_logout.BorderThickness = 2;
+            this.btn_logout.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_logout.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_logout.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_logout.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_logout.FillColor = System.Drawing.Color.Teal;
+            this.btn_logout.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btn_logout.ForeColor = System.Drawing.Color.White;
+            this.btn_logout.Location = new System.Drawing.Point(24, 689);
+            this.btn_logout.Name = "btn_logout";
+            this.btn_logout.Size = new System.Drawing.Size(264, 70);
+            this.btn_logout.TabIndex = 6;
+            this.btn_logout.Text = "Logout";
+            this.btn_logout.Click += new System.EventHandler(this.btn_logout_Click);
+            // 
+            // btn_Custmer
+            // 
+            this.btn_Custmer.Animated = true;
+            this.btn_Custmer.BorderColor = System.Drawing.Color.White;
+            this.btn_Custmer.BorderThickness = 2;
+            this.btn_Custmer.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_Custmer.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_Custmer.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_Custmer.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_Custmer.FillColor = System.Drawing.Color.Teal;
+            this.btn_Custmer.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btn_Custmer.ForeColor = System.Drawing.Color.White;
+            this.btn_Custmer.Location = new System.Drawing.Point(24, 576);
+            this.btn_Custmer.Name = "btn_Custmer";
+            this.btn_Custmer.Size = new System.Drawing.Size(264, 70);
+            this.btn_Custmer.TabIndex = 5;
+            this.btn_Custmer.Text = "Customer";
+            this.btn_Custmer.Click += new System.EventHandler(this.btn_Custmer_Click);
+            // 
+            // btn_products
+            // 
+            this.btn_products.Animated = true;
+            this.btn_products.BorderColor = System.Drawing.Color.White;
+            this.btn_products.BorderThickness = 2;
+            this.btn_products.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_products.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_products.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_products.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_products.FillColor = System.Drawing.Color.Teal;
+            this.btn_products.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btn_products.ForeColor = System.Drawing.Color.White;
+            this.btn_products.Location = new System.Drawing.Point(24, 487);
+            this.btn_products.Name = "btn_products";
+            this.btn_products.Size = new System.Drawing.Size(264, 70);
+            this.btn_products.TabIndex = 4;
+            this.btn_products.Text = "Add Products";
+            this.btn_products.Click += new System.EventHandler(this.btn_products_Click);
+            // 
+            // btn_chashier
+            // 
+            this.btn_chashier.Animated = true;
+            this.btn_chashier.BorderColor = System.Drawing.Color.White;
+            this.btn_chashier.BorderThickness = 2;
+            this.btn_chashier.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_chashier.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_chashier.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_chashier.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_chashier.FillColor = System.Drawing.Color.Teal;
+            this.btn_chashier.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btn_chashier.ForeColor = System.Drawing.Color.White;
+            this.btn_chashier.Location = new System.Drawing.Point(24, 399);
+            this.btn_chashier.Name = "btn_chashier";
+            this.btn_chashier.Size = new System.Drawing.Size(264, 70);
+            this.btn_chashier.TabIndex = 3;
+            this.btn_chashier.Text = "Add Chashier";
+            this.btn_chashier.Click += new System.EventHandler(this.btn_chashier_Click);
+            // 
+            // btn_dashboard
+            // 
+            this.btn_dashboard.Animated = true;
+            this.btn_dashboard.BorderColor = System.Drawing.Color.White;
+            this.btn_dashboard.BorderThickness = 2;
+            this.btn_dashboard.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_dashboard.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_dashboard.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_dashboard.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_dashboard.FillColor = System.Drawing.Color.Teal;
+            this.btn_dashboard.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btn_dashboard.ForeColor = System.Drawing.Color.White;
+            this.btn_dashboard.Location = new System.Drawing.Point(24, 304);
+            this.btn_dashboard.Name = "btn_dashboard";
+            this.btn_dashboard.Size = new System.Drawing.Size(264, 70);
+            this.btn_dashboard.TabIndex = 2;
+            this.btn_dashboard.Text = "Dashboard";
+            this.btn_dashboard.Click += new System.EventHandler(this.btn_dashboard_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(117, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Admins";
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.Image = global::cafeCoffee.Properties.Resources.coffee_cup;
+            this.guna2PictureBox1.ImageRotate = 0F;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(89, 58);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.Size = new System.Drawing.Size(142, 124);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.guna2PictureBox1.TabIndex = 0;
+            this.guna2PictureBox1.TabStop = false;
+            // 
+            // guna2ControlBox2
+            // 
+            this.guna2ControlBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox2.BackColor = System.Drawing.Color.Black;
+            this.guna2ControlBox2.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.guna2ControlBox2.FillColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox2.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox2.Location = new System.Drawing.Point(1308, 3);
+            this.guna2ControlBox2.Name = "guna2ControlBox2";
+            this.guna2ControlBox2.Size = new System.Drawing.Size(54, 43);
+            this.guna2ControlBox2.TabIndex = 1;
+            // 
+            // guna2ControlBox3
+            // 
+            this.guna2ControlBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox3.BackColor = System.Drawing.Color.Black;
+            this.guna2ControlBox3.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MaximizeBox;
+            this.guna2ControlBox3.FillColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox3.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox3.Location = new System.Drawing.Point(1248, 3);
+            this.guna2ControlBox3.Name = "guna2ControlBox3";
+            this.guna2ControlBox3.Size = new System.Drawing.Size(54, 43);
+            this.guna2ControlBox3.TabIndex = 2;
+            // 
+            // panelContainer
+            // 
+            this.panelContainer.BackColor = System.Drawing.Color.White;
+            this.panelContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContainer.Location = new System.Drawing.Point(311, 49);
+            this.panelContainer.Name = "panelContainer";
+            this.panelContainer.Size = new System.Drawing.Size(1114, 904);
+            this.panelContainer.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1425, 953);
+            this.Controls.Add(this.panelContainer);
+            this.Controls.Add(this.panelSilder);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.panelSilder.ResumeLayout(false);
+            this.panelSilder.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox3;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox2;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+        private Guna.UI2.WinForms.Guna2Panel panelSilder;
+        private Guna.UI2.WinForms.Guna2Button btn_logout;
+        private Guna.UI2.WinForms.Guna2Button btn_Custmer;
+        private Guna.UI2.WinForms.Guna2Button btn_products;
+        private Guna.UI2.WinForms.Guna2Button btn_chashier;
+        private Guna.UI2.WinForms.Guna2Button btn_dashboard;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private System.Windows.Forms.Panel panelContainer;
+    }
+}
+
---
+++ cafeCoffee/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cafeCoffee
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            this.WindowState = FormWindowState.Maximized;
+
+        }
+
+        private void AddControler(Control control)
+        {
+            control.Dock = DockStyle.Fill;
+            panelContainer.Controls.Clear();
+            panelContainer.Controls.Add(control);
+            control.BringToFront();
+        }
+
+        private void btn_dashboard_Click(object sender, EventArgs e)
+        {
+            Dashboard dashboard = new Dashboard();
+            AddControler(dashboard);
+        }
+
+        private void btn_chashier_Click(object sender, EventArgs e)
+        {
+            AddChashier addChashier = new AddChashier();
+            AddControler(addChashier);
+        }
+
+        private void btn_products_Click(object sender, EventArgs e)
+        {
+            Products products = new Products();
+            AddControler(products);
+        }
+
+        private void btn_Custmer_Click(object sender, EventArgs e)
+        {
+            Customer customer = new Customer();
+            AddControler(customer);
+        }
+
+        private void close_click(object sender,EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure want to  exit? "," Confirmation Message",MessageBoxButtons.YesNo,MessageBoxIcon.Information);
+            if (result == DialogResult.Yes)
+            {
+
+                Application.Exit();
+            }
+
+        }
+
+        private void btn_logout_Click(object sender, EventArgs e)
+        {
+            DialogResult check = MessageBox.Show("Are you sure you want to  Sing out? ", " Confirmation Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (check == DialogResult.Yes)
+            {
+                Form1 loginForm = new Form1();
+                loginForm.Show();
+                this.Hide();
+            }
+        }
+    }
+}
